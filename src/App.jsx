00b35acace39822bfe8374c38bda26c091c61223
Subhojit1118 <113227443+Subhojit1118@users.noreply.github.com>
2024-02-25 18:22:32 +0530

    overview & signup complet

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Home from "./components/home";
 import Overview from "./components/overview";
-import Demo from "./components/demo";
-import Demo2 from "./components/demo2";
 const App = () => {
   return (
     <>
       <div>
         {/* <Home /> */}
-        {/* <Overview /> */}
-        <Demo2/>
+        <Overview />
       </div>
     </>
   );
